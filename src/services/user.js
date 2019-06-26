7f43074404bca7d033c89d58329922d6dbd5a088
Jefferson Daniel <jeffersondanielss@gmail.com>
2019-06-26 11:21:45 -0300

    feat: validate email and password

--- src/services/user.js
+++ src/services/user.js
@@ -1,10 +1,17 @@
 module.exports = ({ db }) => {
-  const findAll = () => {
-    return db('users').select();
+  const findAll = (filter = {}) => {
+    return db('users').where(filter).select();
   };
 
-  const save = (user) => {
+  const save = async (user) => {
     if (!user.name) return { error: 'Nome é um atributo obrigatório.' };
+    if (!user.mail) return { error: 'Email é um atributo obrigatório.' };
+    if (!user.passwd) return { error: 'Senha é um atributo obrigatório.' };
+
+    const userDB = await findAll({ mail: user.mail });
+
+    if (userDB && userDB.length > 0) return { error: 'Já existe um usuário com este email.' };
+
     return db('users').insert(user, '*');
   };
 
--- test/routes/user.test.js
+++ test/routes/user.test.js
@@ -3,25 +3,61 @@ const { internet } = require('faker');
 
 const app = require('../../src/app');
 
-test('should return all users', async () => {
-  const res = await request(app).get('/users');
+describe('users', () => {
+  beforeAll((done) => {
+    app.db.migrate.latest()
+      .then(() => done());
+  });
 
-  expect(res.status).toBe(200);
-  expect(res.body.length).toBeGreaterThan(0);
-});
+  afterAll((done) => {
+    app.db.migrate.rollback()
+      .then(() => done());
+  });
 
-test('should insert a user with success', async () => {
-  const res = await request(app).post('/users')
-    .send({ name: 'Walter Mitty', mail: internet.email(), passwd: '123' });
+  test('should return all users', async () => {
+    const res = await request(app).get('/users');
 
-  expect(res.status).toBe(201);
-  expect(res.body).toHaveProperty('name', 'Walter Mitty');
-});
+    expect(res.status).toBe(200);
+    expect(res.body.length).toBe(0);
+  });
 
-test('shouldn`t insert user without name', async () => {
-  const res = await request(app).post('/users')
-    .send({ mail: internet.email(), passwd: '123' });
+  test('should insert a user with success', async () => {
+    const res = await request(app).post('/users')
+      .send({ name: 'Walter Mitty', mail: 'walter@mail.com', passwd: '123' });
 
-  expect(res.status).toBe(400);
-  expect(res.body.error).toBe('Nome é um atributo obrigatório.');
+    expect(res.status).toBe(201);
+    expect(res.body).toHaveProperty('name', 'Walter Mitty');
+  });
+
+  test('shouldn`t insert user without name', async () => {
+    const res = await request(app).post('/users')
+      .send({ mail: internet.email(), passwd: '123' });
+
+    expect(res.status).toBe(400);
+    expect(res.body.error).toBe('Nome é um atributo obrigatório.');
+  });
+
+  test('shouldn`t insert user without mail', async () => {
+    const res = await request(app).post('/users')
+      .send({ name: 'Walter Mitty', passwd: '123' });
+
+    expect(res.status).toBe(400);
+    expect(res.body.error).toBe('Email é um atributo obrigatório.');
+  });
+
+  test('shouldn`t insert user without passwd', async () => {
+    const res = await request(app).post('/users')
+      .send({ mail: internet.email(), name: 'Walter Mitty' });
+
+    expect(res.status).toBe(400);
+    expect(res.body.error).toBe('Senha é um atributo obrigatório.');
+  });
+
+  test('shouldn`t insert an mail already existent', async () => {
+    const res = await request(app).post('/users')
+      .send({ mail: 'walter@mail.com', name: 'Walter Mitty', passwd: '123' });
+
+    expect(res.status).toBe(400);
+    expect(res.body.error).toBe('Já existe um usuário com este email.');
+  });
 });
